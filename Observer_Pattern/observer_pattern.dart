287bc61aff2ff7f3f1915f0b5ba993b49270a672
MoSala95 <hangor95@gmail.com>
2021-06-20 15:29:58 +0200

    Observer Pattern First Commit

---
+++ Observer_Pattern/observer_pattern.dart
@@ -0,0 +1,47 @@
+abstract class Observable {
+
+  void registerObserver(Observer observer);
+
+  void notifyObservers();
+
+}
+
+class Observer {
+  String name;
+
+  Observer(this.name);
+
+  void notify() {
+    print("new notification for $name!");
+  }
+}
+
+class NotificationObserver implements Observable {
+
+  List<Observer> ? _observers;
+
+  NotificationObserver([List<Observer>? observers]) {
+    _observers = observers ?? [];
+  }
+
+
+  void registerObserver(Observer observer) {
+    _observers!.add(observer);
+  }
+
+  void notifyObservers() {
+    for (var observer in _observers!) {
+      observer.notify();
+    }
+  }
+
+}
+
+void main() {
+  var me = Observer("me");
+  var mrCoffee = NotificationObserver(List.from([me]));
+  var myFriend = Observer("my friend");
+  mrCoffee.registerObserver(myFriend);
+  mrCoffee.notifyObservers();
+
+}
